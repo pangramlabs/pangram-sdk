01bd47088c9e6d18e762e6a11496cbef4a4fa89b
Max Spero <mmspero@gmail.com>
2024-06-19 13:14:30 -0400

    Workflow to automatically publish python package (#2)
    * add workflow to automatically publish package
    
    * bump version

--- pangram/__init__.py
+++ pangram/__init__.py
@@ -1,5 +1,5 @@
 """Defile all variables to be accessible from `import pangram`."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Max Spero"
 __email__ = "max@pangramlabs.com"
 __license__ = "MIT"
