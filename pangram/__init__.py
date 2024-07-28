3bd1697ea16ccfaf7cbdfb681f95fa8ee15637da
Max Spero <mmspero@gmail.com>
2024-07-28 16:16:47 -0400

    Propogate API errors instead of attempting to read empty responses field (#6)

--- pangram/__init__.py
+++ pangram/__init__.py
@@ -1,5 +1,5 @@
 """Defile all variables to be accessible from `import pangram`."""
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 __author__ = "Max Spero"
 __email__ = "max@pangramlabs.com"
 __license__ = "MIT"
